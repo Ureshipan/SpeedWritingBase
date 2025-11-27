8d4d639f96cb221eaed372a2662650612d47f19f
Tochilkasdf <duyunovsergey@gmail.com>
2025-11-27 21:56:16 +0300

    gjy

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import random
-import os
-import yaml
+from flask import Flask ,request ,jsonify ,render_template 
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime 
+import random 
+import os 
+import yaml 
 
 app =Flask (__name__ )
 app .config ['SQLALCHEMY_DATABASE_URI']='sqlite:///speedwriting.db'
@@ -206,3 +206,5 @@ def init_db ():
 if __name__ =='__main__':
     init_db ()
     app .run (debug =True ,host ='0.0.0.0',port =5000 )
+
+#закончил код
